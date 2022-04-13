8281b39ce4f5e536b45fe6fa3223200d5d5bc29a
tduncan14 <nbatre1@yahoo.com>
2022-04-13 12:09:23 -0700

    keep moving forward

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+
 const auth = require('./routes/auth');
 
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URL,{
+   useNewUrlParser: true})
+.then(() => console.log('database connected'))
+.catch((err) => console.log(err));
 
 
 
@@ -11,7 +21,21 @@ const PORT =  process.env.PORT || 8000
 
 
 app.use(express.json())
+
+//middleware
 app.use('/api',auth)
+
+
+//app middlewares
+
+app.use(morgan('dev'));
+app.use(cors()) // allows all origins
+
+
+// if(process.env.NODE_ENV = 'development'){
+//    app.use(cors({origin:`http://localhost:3000`}))
+// }
+
 
 
 
