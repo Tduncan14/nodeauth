aa0fa0cbcb4aa9677dcd25789b2930001d16ddde
tduncan14 <nbatre1@yahoo.com>
2022-04-13 10:38:18 -0700

    keep moving forward

--- server/models/user.js
+++ server/models/user.js
@@ -39,13 +39,63 @@ const userSchema = new mongoose.Schema({
         default:''
     }
 
+},{timestamps:true})
+
+
+// vitrual and saving the password in the database
+userSchema.virtual('password')
+.set( function(password){
+    this._password = password
+    // generate salt for the password
+    this.salt = this.makeSalt()
+
+    this.hashed_password = this.encryptPassword(password)
 })
-
-
-// vitrual
-
+.get(function(){
+    return this._password
+})
 
 
 
 
 // methods
+userSchema.methods = {
+
+    authenicate:function(plainPassword){
+
+        return this.encryptPassword(plainPassword) = this.hashed_password;
+
+    },
+
+    encryptPassword:function(password){
+        if(!password){
+            return ''
+        }
+
+        try{
+
+            return crypto.createHmac('sha1',secret)
+            .update(password)
+            .digest('hex')
+
+
+        }
+
+        catch(err){
+            return err
+        }
+
+    },
+
+    makeSalt:function(){
+        return Math.round(new Date().valueOf()*Math.random()) + ''
+    }
+
+
+
+
+}
+
+
+
+module.exports = mongoose.model('User',userSchema)
